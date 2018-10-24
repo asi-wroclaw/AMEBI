6c0cf2efcb086cbc2fc0a97e7b0b8e33887c3438
Tomasz Cybulski <tomcyb27@gmail.com>
2018-10-24 23:04:49 +0200

    Added UpdateNuGetPackages scripts

---
+++ UpdateNuGetPackages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Source reference: https://gist.github.com/JonCanning/a083e80c53eb68fac32fe1bfe8e63c48
+regex='PackageReference Include="([^"]*)" Version="([^"]*)"'
+find . -name "*.*proj" | while read proj
+do
+  while read line
+  do
+    if [[ $line =~ $regex ]]
+    then
+      name="${BASH_REMATCH[1]}"
+      version="${BASH_REMATCH[2]}"
+      if [[ $version != *-* ]]
+      then
+        dotnet add $proj package $name
+      fi
+    fi
+  done < $proj
+done
